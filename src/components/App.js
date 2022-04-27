7ac1f50baf54ac3d42a099f4789332fc63f2ee67
nickwells <wellsnick11@yahoo.com>
2022-04-26 20:29:05 -0700

    commit tree cd604a1139938fa577e87bb30a561d99a6949744

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setLisitngs] = useState([]);
+  const [currentSearch, setCurrentSearch] = useState("")
+
+  useEffect(()=> {
+    fetch(" http://localhost:6001/listings")
+    .then((r)=> r.json())
+    .then(setLisitngs)
+  }, [] )
+
+  function handleRemoveListing(id) {
+    const newListings = listings.filter((listing) => listing.id !== id)
+    setLisitngs(newListings)
+  }
+
+    const displayedListings = listings.filter((listing) =>
+    listing.description.toLowerCase().includes(currentSearch.toLowerCase()));
+  
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setCurrentSearch} />
+      <ListingsContainer listings={displayedListings} onRemoveListing={handleRemoveListing} />
     </div>
   );
 }
